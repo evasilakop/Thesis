f9a162e3e4ceaa549d1aee97f522b80da3e37706
evasilakop <eleni.vasilakop@gmail.com>
2025-04-17 18:02:38 +0300

    going along with client.py code to understand mqtt

--- client.py
+++ client.py
@@ -1,22 +1,25 @@
 import time
+import sys
 
 from paho.mqtt import client as mqtt_client
 
 
 #broker = 
-#port = 
+#port = 1883 #default for mqtt
 topic = "weights"
 # Generate a Client ID with the publish prefix.
 #client_id = 
 
 def connect_mqtt():
+    #callback function, gets called after the client gets a message from the broker
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client(client_id)
+    client = mqtt_client.Client(client_id,
+                                protocol = mqtt_client.MQTTv5)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
@@ -25,7 +28,7 @@ def connect_mqtt():
 def publish(client):
     while True:
         time.sleep(1)
-        # msg = 
+        # msg = whatever weight we get back
         result = client.publish(topic, msg)
         # result: [0, 1]
         status = result[0]
@@ -37,9 +40,8 @@ def publish(client):
 
 def run():
     client = connect_mqtt()
-    client.loop_start()
     publish(client)
-    client.loop_stop()
+    client.loop_forever()
 
 
 if __name__ == '__main__':
